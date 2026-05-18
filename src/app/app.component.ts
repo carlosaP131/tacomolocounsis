1889f6b3c59a3ee8e4d16e64cb52618e0161efdd
carlosaP131 <101134690+carlosaP131@users.noreply.github.com>
2026-05-18 05:49:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,NavComponent],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
